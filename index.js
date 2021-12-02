743b8068939ede7ef015e8ec168545bbb4a92dc2
swoyam1234 <sdk.swoyam@gmail.com>
2021-12-02 22:00:43 +0530

    initialized

---
+++ index.js
@@ -0,0 +1,15 @@
+$('.portfolio-item').isotope({
+    // options
+    itemSelector: '.item',
+    layoutMode: 'fitRows'
+  });
+          $('.portfolio-menu ul li').click(function(){
+          $('.portfolio-menu ul li').removeClass('active');
+          $(this).addClass('active');
+  
+              var selector = $(this).attr('data-filter');
+          $('.portfolio-item').isotope({
+              filter:selector
+          });
+          return false;
+      });
